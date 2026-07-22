ad95ee57c057226498104ea5e4d33f34be2a8df6
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-07-22 21:50:44 +0530

    completed variables

--- python/Third_program_variables.py
+++ python/Third_program_variables.py
@@ -17,3 +17,47 @@ print(Age)
 
 AGE=22
 print(AGE)
+
+#Store marks and name in a variable and print those
+name="Soujanya Jain Brahmaraj"
+marks=97
+print( name , marks)
+
+'''
+Task 1
+My name is : python
+My age is : 1
+'''
+name="Python"
+age=1
+print("My name is:", name)
+print("My age is:", age)
+
+'''
+Task 2
+"My name is : Python"
+"My age is : 1"
+"My address is : Netherland"
+'''
+
+name="Python"
+age=1
+address="Netherland"
+print("My name is :",name)
+print("My age is :", age)
+print("My address is:",address)
+
+
+#use of input() function
+name=input("Enter you name")
+age=input("enter your age")
+address=input("enter your address")
+print("my name is: " , name)
+print("my age is: " , age)
+print("my address is : ", address)           
+           
+
+
+
+
+
