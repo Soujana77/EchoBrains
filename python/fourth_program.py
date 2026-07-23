9042daf64d224ec6ce1a63c727f09cc3983ab63a
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-07-23 09:58:43 +0530

    Started with operators and completed arithmatic operators

---
+++ python/fourth_program.py
@@ -0,0 +1,23 @@
+#swaping the values of 2 variables without using using the third variable
+a=2
+b=4
+a,b=b,a
+print(a)
+print(b)
+
+#Operators
+#Arithmatic Operators :(+,-,*,/,//,**)
+
+a=2
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+
+
+
