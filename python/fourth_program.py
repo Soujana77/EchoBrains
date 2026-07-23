66109074d03f5fa00ad489ce9aead5290b3544f1
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-07-23 10:22:15 +0530

    Completed task 1

--- python/fourth_program.py
+++ python/fourth_program.py
@@ -7,17 +7,30 @@ print(b)
 
 #Operators
 #Arithmatic Operators :(+,-,*,/,//,**)
-
+print("Started arithmatic operators")
 a=2
 b=4
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a//b)
-print(a**b)
-print(a%b)
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b",a*b)
+print("a/b",a/b)
+print("a//b",a//b)
+print("a**b",a**b)
+print("a%b",a%b)
 
 
 
+'''
+task get a integer input for variable a,b,c
+multiply all 3 variables(a*b*c)
+add all 3 variable(a+b+c)
+divide the multiplied value by added values print it
+'''
 
+a=int(input("Enter the value of a ="))
+b=int(input("Enter the value of b ="))     
+c=int(input("Enter the value of c ="))
+d=print("product of a*b*c=:",a*b*c)
+e=print("sum of a+b+c=:",a+b+c)
+f=print("the output of values is =",d/e)     
+      
