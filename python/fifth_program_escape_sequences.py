3bb010135e51344b044bcddf2833c5cff5e1b81b
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-07-25 09:53:01 +0530

    started with conditional statements

---
+++ python/fifth_program_escape_sequences.py
@@ -0,0 +1,4 @@
+#Escape Sequences(\n, \r , \t)
+print("hello \n World")
+print("123456\r789")
+print("123456\t1234")      
---
+++ python/sixth_program_conditional statements.py
@@ -0,0 +1,26 @@
+#Conditional statements
+#1.if
+'''
+if condition:
+    statement
+    '''
+age=int(input("Enter your age:"))
+if age>=18:
+    print("you are 18+")
+else:print("bye")
+
+#2.else
+'''
+else:
+    statement
+'''
+#program to check whether a numer is odd or even
+num=int(input("Enter a number: "))
+if num/2!=0:
+    print("the number is odd")
+else:print("the number is even")    
+
+   
+
+
+           
