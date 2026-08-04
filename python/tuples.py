c883dd077a9907e0d7794e492544f5495c330315
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-08-04 10:54:02 +0530

    Started tuples

---
+++ python/tuples.py
@@ -0,0 +1,14 @@
+#TUPlES
+tpl=()#empty tuple without inbuilt
+print(type(tpl))
+tpl=tuple()#Creating a tuple using inbuilt keyword 
+print(type(tpl))
+
+
+a=(1,2.9,"hello",True,5,6)
+print(a)
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
