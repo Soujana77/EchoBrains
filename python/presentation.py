63b4ebde2d99cfe8ef085dfa2d460c5d70a865d8
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-08-06 10:37:03 +0530

    Presentation file on for loop added

---
+++ python/presentation.py
@@ -0,0 +1,39 @@
+#for loop
+
+#for variable in sequence:
+   # statement
+'''
+for i in range(5):
+    print(i)
+
+#range(start,stop,step)
+
+for i in range(5):
+    print(i)
+
+
+for i in range(7):
+    print(i)
+
+   
+
+name="python"
+for letter in name:
+    print(letter)
+ 
+
+fruits=["apple","banana","Mango"]
+for fruit in range(len(fruits)):
+    if fruit==1:
+        print(fruits[1])
+        
+'''
+
+student={
+    "name":"Soujanya",
+    "Age":21,
+    "City":"Harihar"
+    }
+for key,value in student.items():
+    print(key,":",value)
+    
