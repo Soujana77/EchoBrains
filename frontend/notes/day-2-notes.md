570d431bcdb89b25889a4ebca8ee22266457c7db
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-08-11 12:54:12 +0530

    Day-2 HTML done

---
+++ frontend/notes/day-2-notes.md
@@ -0,0 +1,40 @@
+###### Hyperlink:
+
+&#x20;to create a link from one webpage to another it may 
+
+link webpage which is same website (internal link)
+
+or it may link to webpage which is another website (external link)
+
+
+
+###### Anchor Tag
+
+<a href="URL">text/image</a>
+
+
+
+
+
+###### Uniform Resource Locator :
+
+&#x20;Used to find a resource in local server or webserver 
+
+
+
+\*Creating a link on an image , instead of just using the text.
+
+
+
+Standard image file formats in webpage :
+
+.jpg(join photographic group)
+
+.jpeg(join photographic expert group )
+
+.png(portable network graphics)
+
+.gif(graphical inferface format)
+
+.bmp(bit map images)
+
--- python/OOPS.py
+++ python/OOPS.py
@@ -11,10 +11,9 @@ Securing data(encapsulation)
 Handling complexity(abstraction , polymorphism
 '''
 #syntax
-class class_name():
+#class class_name():
     #methods
 #create objects to access class
-
 class A():
     def fun(): #this is a method , a method is dependent on class , but function is indepnedent
         print("Hello")
@@ -22,6 +21,7 @@ class A():
 #To create an object , take any variable and assign class_name to it
 obj=A()
 obj.fun()
+
 
 #Using pass we can create an empty class
 #self is a keyword that says there is a relationship b/w a class and a method 
@@ -34,6 +34,28 @@ obj1=B()
 obj1.add(11,11)
 obj1.sub(9,9)
 
+#Constructor is a special method , as soon as teh object is created the function is called automatically without calling explicitely
+class calc():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+n.calc("Soujj",21)
+
+'''
+create a class called student
+create a variable name and register number using constructor.
+create a function called display which should display the nsme and register nymber of the student
+'''
+
+class student():
+    self.name=name
+    self.register_number=register_number
+    def display(name,register_number):
+        print("Name=",name)
+        print("Register Number =",register_number)
+s.student()
+s.display("Soujj",1234)
 
 
         
