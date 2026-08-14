dbfb98d7dedc3f00c10e127211f97f1ef991f7db
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-08-14 22:22:47 +0530

    added more examples

--- python/method overriding.py
+++ python/method overriding.py
@@ -1,3 +1,4 @@
+#Example 1
 class father():
     def age(self):
         print("i'm 50")
@@ -6,3 +7,57 @@ class son(father):
         print("i'm 50")
 obj=son()
 obj.age()
+
+#Example 2 
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+
+d = Dog()
+d.sound()
+
+#Example 3
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+class Dog(Animal):
+    def sound(self):
+        print("Dog says Bow Bow")
+class Cat(Animal):
+    def sound(self):
+        print("Cat says Meow")
+d = Dog()
+c = Cat():
+
+d.sound()
+d.sound()
+
+#Example 4
+class Student:
+    def __init__(self):
+        print("Student constructor")
+
+class CollegeStudent(Student):
+    def __init__(self):
+        print("College Student constructor")
+
+s = CollegeStudent()
+
+#Example 5
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):
+    def sound(self):
+        super().sound()
+        print("Dog barks")
+
+d = Dog()
+d.sound()
