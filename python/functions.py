e316f63c3ab162aa3f72924a318c3d68b5cafaf1
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-08-15 23:07:55 +0530

    staerted functions

---
+++ python/functions.py
@@ -0,0 +1,14 @@
+#A function is a reusable block of code that performs a particular task.
+#def function_name():
+    # code
+
+#example program-1
+def greet():
+    print("Hello Soujanya")
+    print("Welcome to python")
+greet()
+
+#example program-2
+def hello():
+    print("Hello")
+hello()
