4ccc76d72f6e5977d0464c8c979b85fe50b248b2
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-08-16 21:57:32 +0530

    added more programs on funtions

---
+++ python/More_function_examples.py
@@ -0,0 +1,23 @@
+#function to odd or even
+def check_odd_even(num):
+    if num%2==0:
+        return "Even"
+    else:
+        return "Odd"
+print(check_odd_even(10))
+print(check_odd_even(7))
+
+#Function to find Largest Number
+def largest(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+print(largest(10,20))
+
+#Function With a Loop
+def print_numbers(n):
+    for i in range(1,n+1):
+        print(i)
+print_numbers(5)
+
--- python/functions.py
+++ python/functions.py
@@ -12,3 +12,53 @@ greet()
 def hello():
     print("Hello")
 hello()
+
+#Function with Parameters
+def greet(name):
+    print("Hello",name)
+greet("Soujanya")
+greet("Rahul")
+greet("Anu")
+
+#Parameter vs Argument
+#Parameter → variable in the function definition
+#Argument → actual value passed when calling the function
+def greet(name):       # name → parameter
+    print("Hello",name)
+greet("Soujanya")      #"Soujanya"->argument
+
+#Multiple Parameters
+#example program-1
+def add(a, b):
+    print(a + b)
+add(10, 20)
+
+#example program-2
+def student(name, age, branch):
+    print("Name:", name)
+    print("Age:", age)
+    print("Branch:", branch)
+
+student("Soujanya", 21, "CSE")
+
+#return
+#example program-1
+def add(a, b):
+    print(a + b)
+
+result = add(10, 20)
+print(result)
+
+#Function With Parameters + Return
+def multiply(a, b):
+    result = a * b
+    return result
+
+answer = multiply(5, 4)
+
+print(answer)
+
+#Default Parameters
+def greet(name="Soujanya"):
+    print("Hello", name)
+greet()    
