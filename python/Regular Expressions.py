0e489f2256fea96d3b6d8f4d5625bce5ee5c6281
Soujanya Jain Brahmaraj <varshajanya77@gmail.com>
2026-08-21 10:29:28 +0530

    started regular expressions

---
+++ python/Regular Expressions.py
@@ -0,0 +1,31 @@
+#Regular Expressions ->
+'''
+Finding the pattern in a particular strings,
+matching text searching data validation (email or phone numbers )
+extracting information from large datasets ,
+and formatting or replacing text in files.
+'''
+#we will use packages to implement regular expression
+#A package is a folder containg n number of modules(files) in the folder, the folder must contain
+'''
+search - anywhere
+match - first 
+find - findall
+sub -replace
+'''
+
+import re
+text = "My science marks is 99 and maths marks is 97"
+result = re.findall(r"\d+",text)
+result1 = re.findall(r"\d",text)
+result2 = re.sub(r"\d+", "100",text)
+result3 = re.match("python",text)
+print(result)
+print(result1)
+print(result2)
+print(result3)
+
+#find the fisrt number text = "my age is 25" Use re.search() to find and print 25
+
+
+
